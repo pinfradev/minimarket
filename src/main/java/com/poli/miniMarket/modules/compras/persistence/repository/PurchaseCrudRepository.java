c5e7efb1c52a1ee5cf299a5bc4e8ab028f036da8
daniela <blessingwardrobe@gmail.com>
2023-01-16 22:42:01 -0500

    unused imports

--- src/main/java/com/poli/miniMarket/modules/compras/persistence/repository/PurchaseCrudRepository.java
+++ src/main/java/com/poli/miniMarket/modules/compras/persistence/repository/PurchaseCrudRepository.java
@@ -1,11 +1,7 @@
 package com.poli.miniMarket.modules.compras.persistence.repository;
 
 import com.poli.miniMarket.modules.compras.persistence.entity.PurchaseEntity;
-import jakarta.transaction.Transactional;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
